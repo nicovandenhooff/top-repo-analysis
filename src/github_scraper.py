4ab7ce23aadea64ee1e7f320b9866541b279dbe7
Nico Van den Hooff <nicovandenhooff@gmail.com>
2021-12-29 13:53:12 -0800

    feat: complete scraping script

--- src/github_scraper.py
+++ src/github_scraper.py
@@ -1,11 +1,20 @@
 # Author: Nico Van den Hooff <www.nicovandenhooff.com>
 # License: MIT License
 
+# TODO: Final proofread and code clean up
+
 """
-Implements functions used to scrape data from Github.
+Scrapes the following data from a search query on Github:
+    - The top n repos (max is 1000) as it relates to the search query, where "top" is calculated
+    by total number of stars or forks that a repo has.
+    - The user data for the owners of the above repos.
+    - All of the repository data for the top 25 Github Users for the above repos, where "top" is
+    calculated by the number of followers a Github User has.
+    - All of the repository data for the top 25 Github Organizations for the above repos, where "top"
+    is calculated by the total number of stars an Organization has.
 
 Usage: 
-    github_scraper.py <api_token> [options]
+    github_scraper.py [options]
     github_scraper.py (-h | --help)
 
 Options:
@@ -13,26 +22,31 @@ Options:
     -q <queries> --queries=<queries>  The search queries to scrape Gitub for. If multiple queries
                                       are desired, enclose them with "" and seperate each query 
                                       with a comma. [default: Machine Learning, Deep Learning]
-    -s <sort> --sort=<sort>           How to sort the repos to scrape, must be "stars", "forks",
-                                      or "updated". [default: stars]
+    -s <sort> --sort=<sort>           How to sort the repos to scrape, must be "stars" or "forks".
+                                      [default: stars]
     -o <order> --order=<order>        How to order the repos to scrape, must be "asc" or "desc".
+                                      If "asc" is used then this script scrapes the "bottom" or
+                                      least popular repos, as opposed to the "top" or most popular
+                                      repos for the search queries on Github.
                                       [default: desc] 
     -n <num> --num=<num>              The number of repos to scrape, max is 1000. [default: 1000]
     -p <path> --path=<path>           The output file path to save the scraped data. 
-                                      [default: data/scraped]
+                                      [default: data/scraped/]
 """
 
 import time
+import json
+import pandas as pd
 from docopt import docopt
 from github import Github, GithubException
 
 
-def get_top_repos(g, query, sort="stars", order="desc", n=1000):
+def get_top_repos(g, query, sort, order, num):
     """Gets the top repos for a search query on Github.
 
     Note: The Github API returns a max of 1000 items per search API request.
-          Therefore this method returns the top 1000 repos for a given seach
-          query.  There are "tricks" around this, such as restricting and
+          Therefore this method can only return 1000 repos for a given seach
+          query.  There are "tricks" to increase this, such as restricting and
           bucketing searches by dates, but they are messy and often result in
           the script taking a long time due to API rate limits.
 
@@ -42,12 +56,12 @@ def get_top_repos(g, query, sort="stars", order="desc", n=1000):
         Github object that has been instantiated with an API token.
     query : str
         The query to search Github for.
-    sort : str, by default "stars"
-        How to sort the search results, must be one of ["stars", "forks", "updated"].
-    order : str, by default "desc"
+    sort : str
+        How to sort the search results, must be one of ["stars", "forks"].
+    order : str
         How to arrange the sort, must be one of ["desc", "asc"].
-    n : int, by default 1000
-        The number of repos to scrape, maximum possible is 1000
+    num : int
+        The number of repos to scrape, maximum possible is 1000.
 
     Returns
     -------
@@ -55,14 +69,14 @@ def get_top_repos(g, query, sort="stars", order="desc", n=1000):
         Github data for the top repos for the given search query.
     """
 
-    if n > 1000:
+    if num > 1000:
         print("Error, can only scrape up to 1000 repos")
         return
 
     top_repos = []
 
-    # gets the top 1000 repos, 1 API call
-    repos = g.search_repositories(query, sort=sort, order=order)[:n]
+    # gets the top n repos, 1 API call
+    repos = g.search_repositories(query, sort=sort, order=order)[:num]
 
     # extracts repo information for each repo
     for repo in repos:
@@ -136,7 +150,7 @@ def get_user_data(g, repos):
     return user_data
 
 
-def scrape_github(queries, token, sort="stars", order="desc", n=1000):
+def scrape_github(token, queries, sort, order, num):
     """Scrapes Github repo and user data for a given search query.
 
     Able to process multiple search queries.  If this is desired
@@ -144,25 +158,21 @@ def scrape_github(queries, token, sort="stars", order="desc", n=1000):
 
     Parameters
     ----------
-    query : list, set, or tuple of str
-        The queries to search Github for.
     token : str
         Github API access token.
-    sort : str, by default "stars"
-        How to sort the search results, must be one of ["stars", "forks", "updated"].
-    order : str, by default "desc"
+    queries : list, set, or tuple of str
+        The queries to search Github for.
+    sort : str
+        How to sort the search results, must be one of ["stars", "forks"].
+    order : str
         How to arrange the sort, must be one of ["desc", "asc"].
+    num : int
+        The number of repos to scrape, maximum possible is 1000.
 
     Returns
     -------
     github_data : dict
         Dictionary with k:v pairs as query:(repos, user data)
-
-    Examples
-    --------
-    >>> query = ["Machine Learning", "Deep Learning"]
-    >>> token = "GITHUB_API_TOKEN"
-    >>> github_data = scrape_github(query, token)
     """
 
     if not isinstance(queries, (list, set, tuple)):
@@ -173,23 +183,22 @@ def scrape_github(queries, token, sort="stars", order="desc", n=1000):
     github_data = dict()
 
     for query in queries:
-        top_repos = get_top_repos(g, query, sort=sort, order=order, n=n)
+        top_repos = get_top_repos(g, query, sort=sort, order=order, num=num)
         user_data = get_user_data(g, top_repos)
-
         github_data[query] = (top_repos, user_data)
 
     return github_data
 
 
 def scrape_repos(token, usernames):
-    """Scrapes all of a user or organizations repository data.
+    """Scrapes all of the repository data for a Github User or Organization.
 
     Parameters
     ----------
     token : str
         Github API access token.
     usernames : list, set, or tuple of str
-        The usernames to get repo data for.
+        The Usernames or Organizations to get repo data for.
 
     Returns
     -------
@@ -245,6 +254,8 @@ def check_rate_limit(g):
 
     Note: The core API is limited to 5000 requests per hour.
           The search API is limited to 30 requests per minute.
+          Script is slept at 3 API request remaining for either
+          API in order to be cautious of the limits.
 
     Parameters
     ----------
@@ -252,10 +263,10 @@ def check_rate_limit(g):
         Github object that has been instantiated with an API token.
     """
 
+    # current hour and minute
     t = time.localtime()
     h, m = t[3], t[4]
 
-    # sleep at 3 API requests remaining to be safe
     # search API
     if g.get_rate_limit().search.remaining <= 3:
         print("Search API limit met, sleeping for a minute")
@@ -270,7 +281,96 @@ def check_rate_limit(g):
         print("Sleep completed, resuming script")
 
 
+def get_top_users_and_orgs(user_data_df, top_repos_df):
+    """Gets the top 25 Github Users and top 25 Organizations.
+
+    Note: Top 25 Users are determined based on total followers.
+          Top 25 Organizations are determined based on total stars
+          for all of an organizations repos.
+
+    Parameters
+    ----------
+    user_data_df : pandas DataFrame
+    top_repos_df : pandas DataFrame
+
+    Returns
+    -------
+    tuple of sets
+        The top 25 Github Users and top 25 Organizations
+    """
+    top_users = set(
+        user_data_df.sort_values("followers", ascending=False)
+        .head(25)
+        .query("type == 'User'")["username"]
+        .tolist()
+    )
+
+    top_organizations = set(
+        top_repos_df.query("type == 'Organization'")
+        .groupby("username")
+        .sum()
+        .reset_index()
+        .sort_values("stars", ascending=False)["username"]
+        .head(25)
+        .tolist()
+    )
+
+    return top_users, top_organizations
+
+
+def main(token, queries, sort, order, num, path):
+    """Main function that performs the data scrape"""
+    top_repos_dfs = []
+    user_data_dfs = []
+
+    # gets raw data for top repos and users who own the repos
+    github_data = scrape_github(token, queries, sort, order, num)
+
+    # processes raw data into pandas dfs
+    for query in queries:
+        top_repos = pd.DataFrame(github_data[query][0])
+        user_data = pd.DataFrame(github_data[query][1])
+
+        top_repos["subject"] = query
+        user_data["subject"] = query
+
+        top_repos_dfs.append(top_repos)
+        user_data_dfs.append(user_data)
+
+    top_repos_df = pd.concat(top_repos_dfs).reset_index(drop=True)
+    user_data_df = pd.concat(user_data_dfs).reset_index(drop=True)
+
+    # gets raw data for top 25 users and top 25 orgs and processes into pandas dfs
+    top_users, top_organizations = get_top_users_and_orgs(user_data, top_repos)
+    top_user_repos_df = pd.DataFrame(scrape_repos(token, top_users))
+    top_org_repos_df = pd.DataFrame(scrape_repos(token, top_organizations))
+
+    # saves all the repo data as .csv files
+    top_repos_df.to_csv(f"{path}top-repos.csv", index=False)
+    user_data_df.to_csv(f"{path}user-data.csv", index=False)
+    top_user_repos_df.to_csv(f"{path}top-user-repos.csv", index=False)
+    top_org_repos_df.to_csv(f"{path}top-org-repos.csv", index=False)
+
+
 if __name__ == "__main__":
+
+    # processes command line arguments as required
     opt = docopt(__doc__)
-    print(opt)
-    print(opt["--queries"].split(","))
+    opt["--num"] = int(opt["--num"])
+    if "," in opt["--queries"]:
+        opt["--queries"] = opt["--queries"].split(",")
+
+    # gets github API token
+    with open("src/credentials.json") as f:
+        credentials = json.load(f)
+        api_token = credentials["github_token"]
+
+    # performs data scraping
+    main(
+        api_token,
+        opt["--queries"],
+        opt["--sort"],
+        opt["--order"],
+        opt["--num"],
+        opt["--path"],
+    )
