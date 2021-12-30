4d5d7ee98ada0263ab08cc44cecb89fbf46c0b19
Nico Van den Hooff <nicovandenhooff@gmail.com>
2021-12-30 09:34:49 -0800

    feat: add org chart and docopt to viz script

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -1,14 +1,26 @@
-"""Implements functions to visualized scraped and preprocessed data from Github"""
-
 # Author: Nico Van den Hooff <www.nicovandenhooff.com>
 # License: MIT License
+
+"""
+Visualizes the scraped and preprocessed data from Github
+
+Usage:
+    data_cleaning.py [options]
+    data_cleaning.py (-h | --help)
+
+Options:
+    -h --help                       Show this screen.
+    -i <path> --input_path=<path>   The input file path for the clean scraped data. [default: data/cleaned/]
+    -o <path> --output_path=<path>  The output file path to save the visualizations. [default: results/]
+"""
+
 
 import os
 import numpy as np
 import pandas as pd
 import altair as alt
 import matplotlib.pyplot as plt
-
+from docopt import docopt
 from vega_datasets import data
 from wordcloud import WordCloud
 from github_scraper import get_top_users_and_orgs
@@ -16,6 +28,22 @@ from github_scraper import get_top_users_and_orgs
 
 # allow plotting of large datasets with altair
 alt.data_transformers.enable("data_server")
+
+
+def get_data(input_path):
+    top_repos = pd.read_csv(f"{input_path}top-repos.csv", parse_dates=["created"])
+    user_data = pd.read_csv(f"{input_path}user-data.csv", parse_dates=["created"])
+    location_df = pd.read_csv(
+        f"{input_path}user-location-data.csv", parse_dates=["created"]
+    )
+    top_user_repos = pd.read_csv(
+        f"{input_path}top-user-repos.csv", parse_dates=["created"]
+    )
+    top_org_repos = pd.read_csv(
+        f"{input_path}top-org-repos.csv", parse_dates=["created"]
+    )
+
+    return top_repos, user_data, location_df, top_user_repos, top_org_repos
 
 
 def make_wordcloud(df, column, random_state, colormap, output_path, filename):
@@ -35,10 +63,10 @@ def make_wordcloud(df, column, random_state, colormap, output_path, filename):
 
 def get_worldclouds(top_repos, user_data, output_path):
     make_wordcloud(
-        top_repos, "description", 3, "plasma", output_path, "repo-description-wordcloud"
+        top_repos, "description", 3, "plasma", output_path, "repo_description_wordcloud"
     )
 
-    make_wordcloud(user_data, "bio", 3, "plasma", output_path, "user_bio-wordcloud")
+    make_wordcloud(user_data, "bio", 3, "plasma", output_path, "user_bio_pywordcloud")
 
 
 def top_10_repos_chart(top_repos):
@@ -398,3 +426,132 @@ def get_combined_user_chart(
     )
 
     combined_user_chart.save(os.path.join(output_path, "combined_user_chart.png"))
+
+
+def org_star_chart(top_org_repos, top_repos, user_data):
+
+    _, top_organizations = get_top_users_and_orgs(user_data, top_repos)
+
+    top_org_summary_df = (
+        top_org_repos.drop("id", axis=1)
+        .groupby("username")
+        .agg(["sum", "mean", "median"])
+    )
+
+    top_org_summary_df.columns = top_org_summary_df.columns.map("-".join)
+
+    top_org_summary_df = top_org_summary_df.reset_index()
+
+    top_org_stats_df = user_data.query("username in @top_organizations").merge(
+        top_org_summary_df, on="username"
+    )
+
+    org_star_chart = (
+        alt.Chart(top_org_stats_df, title=alt.TitleParams("Ranked by total stars"))
+        .mark_bar()
+        .encode(
+            alt.X("stars-sum", title="Total stars"),
+            alt.Y("username", sort="x", title="Organization"),
+        )
+    )
+
+    return org_star_chart
+
+
+def org_lang_charts(top_org_repos):
+    org_language_df = top_org_repos[~top_org_repos["language"].isna()]
+    org_language_df = org_language_df.query("language != 'Jupyter Notebook'")
+    lang_order = ["Python", "C#", "C++", "Java", "JavaScript"]
+
+    org_lang_count_chart = (
+        alt.Chart(
+            org_language_df,
+            title=alt.TitleParams("Top 5 most used programming languages"),
+        )
+        .transform_aggregate(count="count()", groupby=["language"])
+        .transform_window(
+            rank="rank(count)", sort=[alt.SortField("count", order="descending")]
+        )
+        .transform_filter(alt.datum.rank <= 5)
+        .mark_bar()
+        .encode(
+            alt.X("count:Q", title="Repositories that implement it"),
+            alt.Y("language", sort="x", title="Language"),
+        )
+    )
+
+    org_language_df = org_language_df.set_index("created")
+    org_language_df["year"] = org_language_df.index.year
+    top_5_languages = org_language_df["language"].value_counts().head(5).index.tolist()
+
+    org_lang_year_chart = (
+        alt.Chart(
+            org_language_df.query("language in @top_5_languages"),
+            title=alt.TitleParams("Use of the top 5 languages from 2009-2013"),
+        )
+        .mark_line(point=True)
+        .encode(
+            alt.X("year", title="Year", axis=alt.Axis(format="y")),
+            alt.Y("count()", title="Repositories using the language"),
+            alt.Color("language", sort=lang_order, title=None),
+        )
+        .properties(height=280)
+    )
+
+    return org_lang_count_chart, org_lang_year_chart
+
+
+def get_combined_org_chart(top_org_repos, top_repos, user_data, output_path):
+
+    org_star = org_star_chart(top_org_repos, top_repos, user_data)
+    org_lang_count, org_lang_year = org_lang_charts(top_org_repos)
+
+    combined_org_chart = (
+        alt.hconcat(
+            org_star,
+            alt.vconcat(org_lang_count, org_lang_year).resolve_scale(
+                color="independent"
+            ),
+            title=alt.TitleParams(
+                text="Organizations implementing ML and DL and the languages they use",
+                dy=-8,
+                fontWeight="lighter",
+                fontSize=20,
+            ),
+        )
+        .configure_axis(labelFontSize=12, titleFontSize=12)
+        .configure_title(fontSize=15, anchor="middle", fontWeight="bold")
+    )
+
+    combined_org_chart.save(os.path.join(output_path, "combined_org_chart.png"))
+
+
+def main(input_path, output_path):
+
+    top_repos, user_data, location_df, top_user_repos, top_org_repos = get_data(
+        input_path
+    )
+
+    print("--Creating wordclouds")
+    get_worldclouds(top_repos, user_data, output_path)
+
+    print("--Creating combined start chart")
+    get_combined_star_chart(top_repos, output_path)
+
+    print("--Creating combined yearly chart")
+    get_combined_yearly_chart(top_repos, output_path)
+
+    print("--Creating combined user chart")
+    get_combined_user_chart(
+        location_df, user_data, top_repos, top_user_repos, output_path
+    )
+
+    print("--Creating combined organization chart")
+    get_combined_org_chart(top_org_repos, top_repos, user_data, output_path)
+
+
+if __name__ == "__main__":
+    print("--Data visualization starting")
+    opt = docopt(__doc__)
+    main(opt["--input_path"], opt["--output_path"])
+    print("--Data visualization complete")
